dbf3c00ca0f4f2a82dfba84be567c06f10aaccc7
Mohammed Zaid Mir <mzaidmir111@gmail.com>
2024-04-16 21:10:12 -0400

    update-Bullet.cs

--- Bullet.cs
+++ Bullet.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : MonoBehaviour {
+    
     [SerializeField] float bulletSpeed = 20f;
     Rigidbody2D myRigidbody;
     PlayerMovement player;
